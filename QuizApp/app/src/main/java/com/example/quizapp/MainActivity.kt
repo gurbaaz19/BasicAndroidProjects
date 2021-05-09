08f04fd279455f60d974711d07b8d6aa701b989f
Gurbaaz Singh Gill <gurbaaz19@gmail.com>
2021-05-10 01:21:07 +0530

    Quiz App- Home Page

--- QuizApp/app/src/main/java/com/example/quizapp/MainActivity.kt
+++ QuizApp/app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.quizapp
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.core.view.WindowCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN //Still used even when its depreciated
+
+    btn_start.setOnClickListener{
+        if(et_name.text.toString().isEmpty()){
+            Toast.makeText(this,"Please enter your name",Toast.LENGTH_SHORT).show()
+        }else{
+            val intent= Intent(this, QuizQuestionsActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
     }
 }
