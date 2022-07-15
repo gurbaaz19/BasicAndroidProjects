290cd17ef825434cf16b59b52f803c000d4b19a6
Gurbaaz Singh Gill <gurbaaz19@gmail.com>
2022-07-15 20:29:50 +0530

    My Places

--- MyPlaces/app/src/main/java/com/example/myplaces/MainActivity.kt
+++ MyPlaces/app/src/main/java/com/example/myplaces/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.myplaces
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         setContentView(R.layout.activity_main)
+
+//        val fabAddPlace=findViewById<com.google.android.material.floatingactionbutton.FloatingActionButton>(R.id.fabAddPlace)
+
+        fabAddPlace.setOnClickListener{
+            val intent = Intent(this, AddPlaceActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
